2085b484c678a2d5b6265718f1698f11ff8cecb0
zekunjin <zekun.jin@foxmail.com>
2023-10-26 11:56:42 +0800

    chore: generate temp files

--- src/build.ts
+++ src/build.ts
@@ -1,31 +1,31 @@
 import { promises as fsp } from 'node:fs'
 import { build as tsupBuild } from 'tsup'
 import { defu } from 'defu'
-import { join, relative } from 'pathe'
+import { basename, extname, join, relative } from 'pathe'
 import fse from 'fs-extra'
 import { globby } from 'globby'
 import Unimport from 'unimport/unplugin'
 import type { CtizenConfig } from './types'
 import { cittyImports } from './imports'
 
-const ENTRYPOINT = (conf: CtizenConfig) => {
+const ENTRYPOINT = async (conf: CtizenConfig) => {
   const srcDir = join(conf.rootDir, conf.srcDir)
   const commandsDir = join(srcDir, 'commands', '*')
-  const paths = globby(commandsDir)
-  console.log(paths)
-  return `
-#!/usr/bin/env node
+  const paths = await globby(commandsDir)
+  const subCommands: Record<string, string> = {}
+
+  paths.forEach((path) => {
+    const key = basename(path, extname(path))
+    subCommands[key] = `import('${path}').then(r => r.default)`
+  })
+
+  return `#!/usr/bin/env node
 import { defineCommand, runMain } from 'citty'
 import { description, name, version } from '../package.json'
-
 const main = defineCommand({
   meta: { name, version, description },
-  subCommands: {
-    dev: () => import('./commands/dev').then(r => r.default),
-    build: () => import('./commands/build').then(r => r.default)
-  }
+  subCommands: { ${Object.entries(subCommands).map(([key, value]) => `${key}: ${value}`).join(',')} }
 })
-
 runMain(main)
 `
 }
@@ -33,7 +33,8 @@ runMain(main)
 export const prepare = async (conf: CtizenConfig) => {
   await prepareDir(conf.buildDir)
   await fse.ensureFile(join(conf.buildDir, 'index.ts'))
-  await fse.writeFileSync(join(conf.buildDir, 'index.ts'), ENTRYPOINT(conf))
+  const entrypoint = await ENTRYPOINT(conf)
+  await fse.writeFile(join(conf.buildDir, 'index.ts'), entrypoint)
 }
 
 const prepareDir = async (dir: string) => {
